b451c713f0c094f9c0804f8a04a7b647a993361d
samashtonroberts <43011326+samashtonroberts@users.noreply.github.com>
2020-08-09 20:29:59 -0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import logging
 from flask import Flask
 from slack import WebClient
 from slackeventsapi import SlackEventAdapter
-from prompt_creator import make_future_prompt
+from prompt_creator import writeprompt
 
 # Initialize a Flask app to host the events adapter
 app = Flask(__name__)
@@ -18,7 +18,7 @@ prompts_sent = {}
 
 def send_prompt(user_id: str, channel: str):
     # Create a new prompt.
-    new_prompt = make_future_prompt()
+    new_prompt = writeprompt()
 
     # Post the new prompt in Slack
     response = slack_web_client.chat_postMessage(**new_prompt)
